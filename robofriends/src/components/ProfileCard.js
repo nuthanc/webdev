74ae405a33e49252f31fc490f10579d125f55eb6
Nuthan Chandra <nuthanc@juniper.net>
2021-02-12 11:52:42 +0530

    Scale on hover

--- robofriends/src/components/ProfileCard.js
+++ robofriends/src/components/ProfileCard.js
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card'
 const ProfileCard = (props) => {
   const { avatar, name, email } = props.profile;
   return (
-    <Card style={{ width: '14rem', backgroundColor: 'lightblue',borderRadius: '5%' }} className="m-3">
+    <Card style={{ width: '14rem', backgroundColor: 'lightblue',borderRadius: '5%' }} className="m-3 card">
       <Card.Img variant="top" src={avatar} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
